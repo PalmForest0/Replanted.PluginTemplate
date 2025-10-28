62e0daa9d8e60e170abd5906e9dacff7f0e514e2
PalmForest <realpalmforest@gmail.com>
2025-10-28 21:42:38 +1100

    Initial commit

---
+++ ReplantedTemplatePlugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace ReplantedTemplate;
+
+[BepInPlugin(Name, Name, Version)]
+public class ReplantedTemplatePlugin : BasePlugin
+{
+    public const string Name = "ReplantedTemplate";
+    public const string Version = "PluginVersion";
+    public const string Id = "PluginId";
+
+    public Harmony Harmony { get; } = new Harmony(Id);
+
+    public static ManualLogSource Logger;
+
+    public override void Load()
+    {
+        Logger = Log;
+        Logger.LogInfo($"\"{Name}\" Plugin has been loaded.");
+
+        Harmony.PatchAll();
+    }
+}
